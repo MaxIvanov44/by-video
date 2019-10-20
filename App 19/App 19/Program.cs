86f7dbd39769ea6a99f48d2adc3cbc0e507cbf06
Максим <44645961+MaxIvanov44@users.noreply.github.com>
2019-10-20 18:26:09 +0300

    Add files via upload

---
+++ App 19/App 19/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace App_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[4];
+            try
+            {
+                Console.WriteLine("Введите строку:  ");
+                string message = Console.ReadLine();
+             
+                if (message.Length > 6)
+                {
+                    throw new Exception("Длина строки > 6 символов");
+                }
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Финал");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ App 20/App 20/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            ref int nRef = ref Find(numbers, 4); //numbers[3]
+            Console.WriteLine(numbers[3]);
+            nRef = 45;
+            Console.WriteLine(numbers[3]);
+
+            Console.ReadKey();
+        }
+        static ref int Find(int[] numbers, int number)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == number)
+                {
+                    return ref numbers[i];
+                }
+            }
+            throw new Exception("число не найдено");
+        }
+    }
+}
