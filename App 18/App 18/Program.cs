5f9f9efbc7c750505cde57cd1cb88026b3f18fac
Максим <44645961+MaxIvanov44@users.noreply.github.com>
2019-10-20 15:46:08 +0300

    Add files via upload

---
+++ App 18/App 18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace App_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static (string, int) GetValue((string,int) tuple, int x)
+            {
+                var result = (name: tuple.Item1, age: tuple.Item2 +x);
+;
+                return result;
+            }
+            var tuple = GetValue(("Max",18), 1);
+            Console.WriteLine(tuple.Item1);
+            Console.WriteLine(tuple.Item2);
+
+            Console.ReadKey();
+        }
+    }
+}
