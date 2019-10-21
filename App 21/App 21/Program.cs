baac8e70bb6fbe3ae0023a2afb6e6d91da9fea5b
Максим <44645961+MaxIvanov44@users.noreply.github.com>
2019-10-21 18:09:47 +0300

    Add files via upload

---
+++ App 21/App 21/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace App_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User max = new User { age = 99, name  ="MAX" };
+            max.Info();
+
+            User bob = new User("Bob") { age = 99};
+            bob.Info();
+
+            Console.ReadKey();
+        }
+      
+    }
+}
---
+++ App 21/App 21/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_21
+{
+  
+       public  class User
+        {
+            public string name;
+            public int age;
+            public User()
+            {
+
+            }
+            public User(string name)
+            {
+                this.name = name;
+            }
+            public User(string n, int a) : this()
+            {
+                age = a;
+            }
+
+            public void Info()
+            {
+                Console.WriteLine(name + "  " + age);
+            }
+        }
+    
+}
